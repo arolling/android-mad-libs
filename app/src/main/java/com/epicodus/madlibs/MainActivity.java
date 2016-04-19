0f3b03bf288db1637df67d84ffacb7f915c18f7d
Abby Rolling and Daren Schaad <arolling@gmail.com>
2016-04-19 09:51:34 -0700

    Refactor with butterknife

--- app/src/main/java/com/epicodus/madlibs/MainActivity.java
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -3,22 +3,30 @@ package com.epicodus.madlibs;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mBtnCreateStory;
+    public static final String TAG = MainActivity.class.getSimpleName();
+    @Bind(R.id.btnCreateStory) Button mBtnCreateStory;
+    @Bind(R.id.tfPlace) EditText mTfPlace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mBtnCreateStory = (Button) findViewById(R.id.btnCreateStory);
+        ButterKnife.bind(this);
 
         mBtnCreateStory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
+                String place = mTfPlace.getText().toString();
+                Log.d(TAG, place);
                 Intent intent = new Intent(MainActivity.this, StoryActivity.class);
                 startActivity(intent);
             }
