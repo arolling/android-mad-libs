022a12671a5bf6fdafa75b4cbc1e964dbba6974b
Abby Rolling and Daren Schaad <darenschaad@gmail.com>
2016-04-19 14:57:19 -0700

    passing unit test to detect the header

--- app/src/test/java/com/epicodus/madlibs/MainActivityTest.java
+++ app/src/test/java/com/epicodus/madlibs/MainActivityTest.java
@@ -1,7 +1,35 @@
 package com.epicodus.madlibs;
 
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertTrue;
+
 /**
  * Created by Guest on 4/19/16.
  */
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
 public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextHeader() {
+        TextView appHeader = (TextView) activity.findViewById(R.id.appHeader);
+        assertTrue("Fill in the Blanks!".equals(appHeader.getText().toString()));
+    }
 }
