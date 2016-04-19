2acc146572f1255f93239dd2874efd94fc7ea51f
Abby Rolling and Daren Schaad <darenschaad@gmail.com>
2016-04-19 16:34:22 -0700

    Add test for spinner selection

--- app/src/androidTest/java/com/epicodus/madlibs/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/epicodus/madlibs/MainActivityInstrumentationTest.java
@@ -5,13 +5,19 @@ import android.support.test.rule.ActivityTestRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.containsString;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 
 
 /**
@@ -33,5 +39,20 @@ public class MainActivityInstrumentationTest {
         onView(withId(R.id.tfPlace)).perform(typeText(place));
         onView(withId(R.id.btnCreateStory)).perform(click());
         onView(withId(R.id.storyHolder)).check(matches(withText(containsString("Mexico"))));
+    }
+
+    @Test
+    public void spinnerIsDisplayed(){
+        onView(withId(R.id.spinner)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void selectedSpinnerIsSelected() {
+        String place = "Mexico";
+        onView(withId(R.id.tfPlace)).perform(typeText(place));
+        onView(withId(R.id.spinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Coding"))).perform(click());
+        onView(withId(R.id.btnCreateStory)).perform(click());
+        onView(withId(R.id.tvResultsHeader)).check(matches(withText(containsString("Coding"))));
     }
 }
