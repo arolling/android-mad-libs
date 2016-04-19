5a5f6cdaa100221737b9bb5e2b742265cdad7cc2
Abby Rolling and Daren Schaad <arolling@gmail.com>
2016-04-19 10:17:32 -0700

    Used butterknife to bind data and show it on our story activity page

--- app/src/main/java/com/epicodus/madlibs/MainActivity.java
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -15,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     @Bind(R.id.btnCreateStory) Button mBtnCreateStory;
     @Bind(R.id.tfPlace) EditText mTfPlace;
+    @Bind(R.id.tfAdjective1) EditText mTfAdjective1;
+    @Bind(R.id.tfAdjective2) EditText mTfAdjective2;
+    @Bind(R.id.tfFemCeleb) EditText mTfFemCeleb;
+    @Bind(R.id.tfMalCeleb) EditText mTfMalCeleb;
+    @Bind(R.id.tfNoun1) EditText mTfNoun1;
+    @Bind(R.id.tfNoun2) EditText mTfNoun2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +34,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 String place = mTfPlace.getText().toString();
-                Log.d(TAG, place);
+                String adjective1 = mTfAdjective1.getText().toString();
+                String adjective2 = mTfAdjective2.getText().toString();
+                String femCeleb = mTfFemCeleb.getText().toString();
+                String malCeleb = mTfMalCeleb.getText().toString();
+                String noun1 = mTfNoun1.getText().toString();
+                String noun2 = mTfNoun2.getText().toString();
                 Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+                intent.putExtra("place", place);
+                intent.putExtra("adjective1", adjective1);
+                intent.putExtra("adjective2", adjective2);
+                intent.putExtra("femCeleb", femCeleb);
+                intent.putExtra("malCeleb", malCeleb);
+                intent.putExtra("noun1", noun1);
+                intent.putExtra("noun2", noun2);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/epicodus/madlibs/StoryActivity.java
+++ app/src/main/java/com/epicodus/madlibs/StoryActivity.java
@@ -1,13 +1,32 @@
 package com.epicodus.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class StoryActivity extends AppCompatActivity {
+    public static final String TAG = StoryActivity.class.getSimpleName();
+    @Bind(R.id.storyHolder) TextView mStoryHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String place = intent.getStringExtra("place");
+        String adjective1 = intent.getStringExtra("adjective1");
+        String adjective2 = intent.getStringExtra("adjective2");
+        String femCeleb = intent.getStringExtra("femCeleb");
+        String malCeleb = intent.getStringExtra("malCeleb");
+        String noun1 = intent.getStringExtra("noun1");
+        String noun2 = intent.getStringExtra("noun2");
+        mStoryHolder.setText("Things entered: " + place + adjective1 + adjective2 + femCeleb + malCeleb + noun1 + noun2);
+
     }
 }
