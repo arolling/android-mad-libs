0841d25ceef8636a223e360bff472aa281cee6b0
Abby Rolling and Daren Schaad <darenschaad@gmail.com>
2016-04-19 15:34:13 -0700

    write passing tests for spinner and list of inputs

--- app/src/test/java/com/epicodus/madlibs/MainActivityTest.java
+++ app/src/test/java/com/epicodus/madlibs/MainActivityTest.java
@@ -1,6 +1,9 @@
 package com.epicodus.madlibs;
 
 import android.os.Build;
+import android.text.Layout;
+import android.widget.Spinner;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import org.junit.Before;
@@ -10,6 +13,8 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 
 /**
@@ -21,15 +26,31 @@ import static junit.framework.Assert.assertTrue;
 
 public class MainActivityTest {
     private MainActivity activity;
+    private Spinner mStorySpinner;
+    private TableLayout mmmTable;
 
     @Before
     public void setup() {
         activity = Robolectric.setupActivity(MainActivity.class);
+        mStorySpinner = (Spinner) activity.findViewById(R.id.spinner);
+        mmmTable = (TableLayout) activity.findViewById(R.id.tableLayout);
     }
 
     @Test
     public void validateTextHeader() {
         TextView appHeader = (TextView) activity.findViewById(R.id.appHeader);
         assertTrue("Fill in the Blanks!".equals(appHeader.getText().toString()));
+    }
+
+    @Test
+    public void storyListViewPopulates() {
+        assertNotNull(mStorySpinner.getAdapter());
+        assertEquals(mStorySpinner.getAdapter().getCount(), 2);
+    }
+
+    @Test
+    public void blankInputsAppearCorrectly(){
+        assertNotNull(mmmTable.getChildCount());
+        assertEquals(mmmTable.getChildCount(), 13);
     }
 }
